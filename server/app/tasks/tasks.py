235ebe6366a6c5278fc04978ac3832347297d8ed
Kevin Glover-Netherton <kevin.glovernetherton@gmail.com>
2019-10-14 15:44:07 -0400

    Minor changed to logic so same category not added multiple times

--- server/app/tasks/tasks.py
+++ server/app/tasks/tasks.py
@@ -36,7 +36,7 @@ def process_link(link):
 
 @celery.task
 def request_html(message):
-    print("Step 1")
+    print(f"Step 1: processing {message['url']}")
     r = requests.get(message["url"])
     if r.status_code == 200:
         print("Retrieved HTML successfully")
@@ -50,7 +50,7 @@ def request_html(message):
 
 @celery.task
 def get_content_from_html(message):
-    print("Step 2")
+    print("Step 2 - parsing HTML")
     html_content = BeautifulSoup(message["html_content"], 'html.parser')
     body_content = html_content.body
     # Remove JS, CSS, and code blocks. Also remove header, and nav since those likely won't be relevant
@@ -62,7 +62,7 @@ def get_content_from_html(message):
 
 @celery.task
 def get_keywords_from_content(message):
-    print("Step 3")
+    print("Step 3 - extracting keywords")
     pos = {'NOUN', 'PROPN', 'ADJ', 'VERB'}
 
     extractor = pke.unsupervised.TopicRank()
@@ -79,8 +79,9 @@ def get_keywords_from_content(message):
 
 @celery.task
 def categorize_entity(message):
-    print("Step 4")
+    print("Step 4 - matching keywords")
     key_dict = map_keywords_to_dict(message["keywords"])
+    print(key_dict)
     user_categories = Category.query.filter_by(user_id=message["uuid"]).all()
 
     link = Link.query.get(message["link_id"])
@@ -88,8 +89,10 @@ def categorize_entity(message):
     for category in user_categories:
         for cat_keyword in category.keywords:
             if cat_keyword.keyword in key_dict.keys():
-                print("Found a match")
-                category.links.append(link) # should set up relationship correctly
+                print(f"Found matching keyword - {cat_keyword.keyword}")
+                category.links.append(link)  # should set up relationship correctly
+                break  # To outer loop
+    # change state flag to processed
 
     db.session.commit()
     return message
