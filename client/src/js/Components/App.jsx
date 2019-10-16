051a835daf6091cf5b2c2b885d294b4221769576
Sid <sid@Sids-MacBook-Pro.local>
2019-10-16 14:20:59 -0400

    Added sign up

--- client/src/js/Components/App.jsx
+++ client/src/js/Components/App.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import '../../css/App.css';
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import Login from './Login';
+import SignUp from './SignUp';
 import DashBoard from './Dashboard';
 import { Provider } from 'react-redux'
 import store from '../../store'
-import { PrivateRoute, LoginRoute } from '../Utility/Routes';
+import { PrivateRoute, LoginRoute, SignUpRoute } from '../Utility/Routes';
 
 class App extends React.Component {
   render() {
@@ -14,6 +15,7 @@ class App extends React.Component {
         <Router>
           <Switch>
             <LoginRoute authed={false} path="/login" component={Login} />
+            <SignUpRoute authed={false} path="/signup" component={SignUp} />
             <PrivateRoute authed={false} path='/' component={DashBoard} />
           </Switch>
         </Router>
