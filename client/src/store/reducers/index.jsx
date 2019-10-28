94fb2e73dca1c5b66cd92389c5a19f2e5136dea7
Abdul Basit <brcoopbasit@gmail.com>
2019-10-28 10:25:37 -0400

    remove unused file

--- client/src/store/reducers/index.jsx
+++ client/src/store/reducers/index.jsx
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import category from './category';
-import user from './user';
 import link from './link';
 
 export default combineReducers({
     category,
-    user,
     link
 })
