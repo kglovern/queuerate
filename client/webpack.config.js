b70e0a90cb2b477f5805217c8d8cecbafee933ba
Sid <sid@Sids-MacBook-Pro.local>
2019-11-10 19:45:18 -0500

    Update webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,7 +5,7 @@ var HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/js/index.js",
   output: {
-    // path: __dirname + '/dist',
+    path: "/",
     publicPath: "/",
     filename: "./bundle.js"
   },
