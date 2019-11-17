53dae09ae68f7fd025cdc252a2d01556ee405cde
Daniel <dkolodzi@mail.uoguelph.ca>
2019-11-17 18:41:04 -0500

    frontend mockup for forwarding settings

--- client/src/js/Components/UserSettings/ForwardingSetting.jsx
+++ client/src/js/Components/UserSettings/ForwardingSetting.jsx
@@ -6,6 +6,10 @@ import FormControl from "@material-ui/core/FormControl";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import SaveIcon from '@material-ui/icons/Save';
 
 
 const ForwardingSettingView = ({ forwardingSetting }) => {
@@ -15,6 +19,7 @@ const ForwardingSettingView = ({ forwardingSetting }) => {
         2: "Pocket",
         3: "Instapaper"
     }
+    const [it, setIT] = useState(fs.forwarding_app || 1)
     console.log(forwardingSetting)
 
     const alignment = {
@@ -38,18 +43,34 @@ const ForwardingSettingView = ({ forwardingSetting }) => {
         <div>
             <div style={alignment}>
                 <h2>
-                    {integrationTypes[fs.forwarding_app] || "Unknown"} Forwarding Settings
+                    Forwarding Settings
                 </h2>
             </div>
 
             {
-                <Paper>
-                    <FormControl>
-                            {fs.default_forwarding_url}
-                        <TextField>
-                        </TextField>
-                    </FormControl>
-                </Paper>
+                //<Paper>
+                <FormControl>
+                    <InputLabel id="demo-simple-select-label">Integration Type</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={it || "Unknown"}
+                    >
+                        {
+                            Object.keys(integrationTypes).map(key => (
+                                <MenuItem value={key} key={key}>{integrationTypes[key]}</MenuItem>
+                            ))
+                        }
+                    </Select>
+                    <TextField label="API Key" variant="filled" defaultValue={fs.api_key}>
+                    </TextField>
+                    <TextField label="Forwarding URL" variant="filled" defaultValue={fs.default_forwarding_url}>
+                    </TextField>
+                    <Button variant="contained" startIcon={<SaveIcon/>} >
+                        Save
+                    </Button>
+                </FormControl>
+                //</Paper>
             }
         </div>
     );
