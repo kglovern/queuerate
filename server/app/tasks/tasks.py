d0a7bf07d34e39d95743279c4cfe2ca5ec744715
Kevin Glover-Netherton <kevin.glovernetherton@gmail.com>
2019-11-24 18:05:04 -0500

    Added RelevantKeyword model, added route to receive them for a list - will change task to store keywords afterwards

--- server/app/tasks/tasks.py
+++ server/app/tasks/tasks.py
@@ -100,7 +100,9 @@ def categorize_entity(message):
 
     link = Link.query.get(message["link_id"])
 
-    # Insert all relevant keywords for a link -false as default, we can make them true later
+    # Insert all relevant keywords for a link - false as default, we can make them true later
+    link.relevant_keywords = []  # Reset keywords so they aren't duplicated when re-run
+    db.session.commit()
     for keyword in key_dict.keys():
         rk = RelevantKeyword(keyword=keyword,
                              link_id=link.id,
